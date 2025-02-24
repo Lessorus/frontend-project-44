5c519d9d1fb3fae6b8688c2cd4c1ec6dbbccc1d4
Lessorus <urmatzarlykov555@gmail.com>
2025-02-24 13:52:43 +0700

    2st

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const game = () => {
+  console.log('Welcome to the Brain Games');
+  const name = readlineSync.question('Please enter your name ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default game;
