427d9e3fe940a2d53bd49388997e76890ce28cda
Lessorus <urmatzarlykov555@gmail.com>
2025-02-24 12:28:22 +0700

    1st

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+export default [
+    {
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...globals.jest,
+        },
+        parserOptions: {
+          // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+          // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+          ecmaVersion: 'latest',
+          sourceType: 'module',
+        },
+      },
+      plugins: { import: importPlugin },
+      rules: {
+        ...importPlugin.configs.recommended.rules,
+      },
+    },
+    ...compat.extends('airbnb-base'),
+    {
+      rules: {
+        'no-underscore-dangle': [
+          'error',
+          {
+            allow: ['__filename', '__dirname'],
+          },
+        ],
+        'import/extensions': [
+          'error',
+          {
+            js: 'always',
+          },
+        ],
+        'import/no-named-as-default': 'off',
+        'import/no-named-as-default-member': 'off',
+        'no-console': 'off',
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ];
